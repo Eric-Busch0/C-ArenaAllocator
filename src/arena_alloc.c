b8ed90d9802ea7bb5539b8cd76ba057b19cee04c
Alpacabet <49300674+Eric-Busch0@users.noreply.github.com>
2023-05-21 18:25:32 -0500

    format

--- src/arena_alloc.c
+++ src/arena_alloc.c
@@ -1,31 +1,27 @@
 #include "arena_alloc.h"
 #include <stdlib.h>
 
-
 int arena_create(arena_t *arena, size_t arena_size)
 {
     arena->start = malloc(arena_size);
     arena->idx = 0;
     arena->size = arena_size;
-    
+
     return arena->start == NULL ? -1 : 0;
 }
-void * arena_alloc(arena_t *arena, size_t size)
+void *arena_alloc(arena_t *arena, size_t size)
 {
 
-    if(arena->idx >= arena->size)
+    if (arena->idx >= arena->size)
     {
         return NULL;
     }
 
     void *mem = arena->start + arena->idx;
 
-
     arena->idx += size;
 
-
     return mem;
-
 }
 void arena_free(arena_t *arena)
 {
